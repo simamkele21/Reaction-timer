f508a99f1acf50b2fa7213bc4d921666750018b4
Simamkele21 <simamkelejanuary@gmail.com>
2022-01-21 14:58:46 +0200

    first commit

---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>Reaction time - {{ score }}</p>
+    <p class="speed">{{ speed }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      speed: null,
+    };
+  },
+  mounted() {
+    console.log(this.score, "ms");
+    if (this.score < 250) {
+      this.speed = "very fast";
+    } else if (this.score >= 250 && this.score < 400) {
+      this.speed = "Relatively fast";
+    } else {
+      this.speed = "Not so fast";
+    }
+  },
+};
+</script>
+
+<style>
+.speed {
+  font-size: 1.4er;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
